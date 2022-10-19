71d5aeb434657086f09812de5dbf729a568da7b6
SomeStupidSongBird <563ftw@gmail.com>
2022-10-18 20:56:32 -0400

    mostly functional

--- getCourses.py
+++ getCourses.py
@@ -8,5 +8,7 @@ headers = {'Authorization': bearer}
 response = requests.get(url, headers=headers)
 print('My courses:')
 data = json.loads(response.content)
+
 for course in data:
-    print(course['name'])
+    col = list(course.keys())[1]
+    print(course[col])
